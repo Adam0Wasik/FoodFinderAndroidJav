7566adbda6319f45c61a59f0c0f6f5fa166f4f6b
AdamW <58723605+KwasikSasik@users.noreply.github.com>
2021-02-12 16:27:51 +0100

    oki

--- app/src/main/java/com/example/foods/MainActivity.java
+++ app/src/main/java/com/example/foods/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.foods;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,13 +21,39 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextView distValue, priceValue, tagBox;
     private SeekBar priceBar, distBar;
     private Toolbar toolbar;
-
+    private SupportMapFragment supporMapFragment;
+    private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private double currentLat = 0;
+    private double currentLong = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,7 +120,76 @@ public class MainActivity extends AppCompatActivity {
               tagBox.setText("");
             }
         });
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Button searchBtn = (Button) findViewById(R.id.srchButton);
+        searchBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float radius = distBar.getProgress();
+               // String type = (String) tagBox.getText();
+                String currLat = Double.toString(currentLat);
+                String currLng = Double.toString(currentLong);
+
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+                        "location=" + currLat + "," + currLng +
+                        "&radius= 5000"  +
+
+                        "&sensor=true" +
+                        "&key=" + getResources().getString(R.string.google_map_api_key);
+                new PlaceTask().execute(url);
+
+
+            }
+        });
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        supporMapFragment = (SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        //place types
+        String[] placeTypeList = { "atm", "bank","hospital"};
+        String[] placeNameList = { "Atm", "Bank","Hospital"};
+
+        //FusedLocationProvider Client
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //check permissions
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //When Granted
+            getCurrentLocation();
+        }else{
+            //Request Permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
 }
+
+    private void getCurrentLocation() {
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+               //when succes
+
+                if (location != null){
+                    currentLat = location.getLatitude();
+                    currentLong = location.getLongitude();
+                    supporMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            Toast.makeText(MainActivity.this, "map zoomed", Toast.LENGTH_SHORT).show();
+                            map = googleMap;
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                  new LatLng(currentLat,currentLong),10
+                            ));
+                        }
+                    });
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,5 +210,82 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 44){
+            if( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                 data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            new PareserTask().execute(s);
+        }
+    }
+
+    private String downloadUrl(String string)throws IOException {
+        URL url = new URL(string);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line  = "";
+        while((line = reader.readLine()) != null){
+            builder.append(line);
+        }
+        String data = builder.toString();
+        reader.close();
+        return data;
+    }
+
+    private class PareserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>>{
+        @Override
+        protected  List<HashMap<String,String>> doInBackground(String... strings) {
+            JsonPareser jsonPareser = new JsonPareser();
+            List<HashMap<String,String>> mapList = null;
+            JSONObject object = null;
+            try {
+                object = new JSONObject(strings[0]);
+                mapList = jsonPareser.parseResult(object);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+             map.clear();
+             for(int i = 0; i<hashMaps.size(); i++){
+                 HashMap<String,String> hashMapList = hashMaps.get(i);
+                 double lat = Double.parseDouble(hashMapList.get("lat"));
+                 double lng = Double.parseDouble(hashMapList.get("lng"));
+                 String name = hashMapList.get("name");
+                 LatLng latLng = new LatLng(lat,lng);
+                 MarkerOptions options = new MarkerOptions();
+                 options.position(latLng);
+                 options.title(name);
+                 map.addMarker(options);
+                 Toast.makeText(MainActivity.this, "map  updt", Toast.LENGTH_SHORT).show();
+             }
+        }
     }
 }
